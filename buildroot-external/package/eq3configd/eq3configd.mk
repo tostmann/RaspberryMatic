743494ec1980cdb6ee9476988f05b7fd13e93fef
Jens Maus <mail@jens-maus.de>
2020-06-23 23:28:27 +0200

    added first service user for securing the eq3configd daemon.

--- buildroot-external/package/eq3configd/eq3configd.mk
+++ buildroot-external/package/eq3configd/eq3configd.mk
@@ -4,12 +4,17 @@
 #
 #############################################################
 
-EQ3CONFIGD_VERSION = 1.0.0
+EQ3CONFIGD_VERSION = 1.1.0
 EQ3CONFIGD_SITE = $(BR2_EXTERNAL_EQ3_PATH)/package/eq3configd
 EQ3CONFIGD_SITE_METHOD = local
 
-define EQ3CONFIGD_INSTALL_TARGET_CMDS
-	cp -a $(@D)/rootfs-overlay/* $(TARGET_DIR)/
+define EQ3CONFIGD_USERS
+  eq3configd -1 eq3configd -1 * - - - eQ3 config daemon
+endef
+
+define EQ3CONFIGD_INSTALL_INIT_SYSV
+  $(INSTALL) -m 0755 -D $(@D)/S50eq3configd \
+    $(TARGET_DIR)/etc/init.d/S50eq3configd
 endef
 
 $(eval $(generic-package))
